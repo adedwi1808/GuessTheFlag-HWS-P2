fcf7e39c9242258182e7cc188539ec39b6b46a1e
adedwi1808 <adedwip1808@gmail.com>
2022-09-27 08:17:07 +0700

    Initial Commit

---
+++ GuessTheFlag-WHS-P2/GuessTheFlag_WHS_P2App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag_WHS_P2App.swift
+//  GuessTheFlag-WHS-P2
+//
+//  Created by Ade Dwi Prayitno on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlag_WHS_P2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
